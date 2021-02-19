329f2919889564e495d5fcfba1cd3917591b0e34
Wesley Lopes <x.wesleylopes@gmail.com>
2021-02-19 19:41:30 -0300

    refactor: create a default settings object

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,7 +6,7 @@ function WorkApp () {
     stopButton: document.querySelector('#stop-button')
   }
 
-  const currentSettings = {
+  const defaultSettings = {
     time: {
       minutes: 25,
       seconds: 0
@@ -14,9 +14,11 @@ function WorkApp () {
     isPlaying: false
   }
 
+  const currentSettings = { ...defaultSettings }
+
   function resetTimer () {
-    currentSettings.time = { minutes: 25, seconds: 0 }
-    currentSettings.isPlaying = false
+    currentSettings.time = { ...defaultSettings.time }
+    currentSettings.isPlaying = defaultSettings.isPlaying
 
     updateButtons()
     updateDisplayTimer()
@@ -45,11 +47,15 @@ function WorkApp () {
 
   function formatTime (time) {
     const numberTime = Number(time)
-    return numberTime > 10 ? numberTime : `0${numberTime}`
+    return numberTime >= 10 ? numberTime : `0${numberTime}`
   }
 
   function runTimerInterval () {
     setInterval(() => {
+      if (currentSettings.time.minutes <= 0 && currentSettings.time.seconds <= 0) {
+        resetTimer()
+      }
+
       if (!currentSettings.isPlaying) return false
 
       currentSettings.time.seconds -= 1
@@ -81,7 +87,7 @@ function WorkApp () {
     const circlePercentage = percentageContainer.querySelector('svg circle')
     const fullCircleLength = circlePercentage.getAttribute('stroke-dasharray')
 
-    const percentage = Number(fullCircleLength) * timeToSeconds(currentSettings.time) / timeToSeconds({ minutes: 25, seconds: 0 })
+    const percentage = Number(fullCircleLength) * timeToSeconds(currentSettings.time) / timeToSeconds({ ...defaultSettings.time })
     circlePercentage.setAttribute('stroke-dashoffset', percentage)
   }
 
